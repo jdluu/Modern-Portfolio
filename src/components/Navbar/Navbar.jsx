9af99fedd6b5d269ab35cb41a45cf5f262725c38
Jeffrey Luu <83382029+jdluu@users.noreply.github.com>
2023-04-05 22:38:03 -0700

    style navbar and modify navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,34 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import site from '../../data/site.json';
-const {nav} = site;
-import './Navbar.css';
+import React, { useState, useEffect } from "react";
+import site from "../../data/site.json";
+const { nav } = site;
+import "./Navbar.css";
 
 const Navbar = () => {
-    const [color, setColor] = useState(false);
+	const [color, setColor] = useState(false);
 
-    useEffect(() => {
-        window.addEventListener('scroll', () => handleScroll);
-        return() => window.removeEventListener('scroll', handleScroll);
-    }, []);
+	useEffect(() => {
+		window.addEventListener("scroll", () => handleScroll);
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, []);
 
-    function handleScroll() {
-        if(window.scrollY > 50) {
-            setColor(true);
-        } else {
-            setColor(false);
-        }
-    }
+	function handleScroll() {
+		if (window.scrollY > 50) {
+			setColor(true);
+		} else {
+			setColor(false);
+		}
+	}
 
-    return (
-        <header id="header" className={color ? 'active' : ''}>
-            <ul>
-              {nav.map((item, i) => (
-                <li key={i}>
-                    <a href={item.link} target={item.target} rel="noopener noreferrer">{item.name}</a>
-                </li>
-              ))}
-            </ul>
-        </header>
-      )
-}
-export default Navbar; 
+	return (
+		<header id="header" className={color ? "active" : ""}>
+			<ul>
+				{nav.map((item, i) => (
+					<li key={i}>
+						<a href={item.link} target={item.target} rel="noopener noreferrer">
+							{item.name}
+						</a>
+					</li>
+				))}
+			</ul>
+		</header>
+	);
+};
+export default Navbar;
