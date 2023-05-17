a368e7783de3a4ecdf65b604a171b467b41eb695
Jeffrey Luu <83382029+jdluu@users.noreply.github.com>
2023-05-16 17:31:00 -0700

    optimize theme switching js

--- public/scripts/theme.js
+++ public/scripts/theme.js
@@ -1,29 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
 	const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
+	const currentTheme = localStorage.getItem("theme");
 
-	//Track the theme switch button
+	// Function to switch the theme
 	function switchTheme(e) {
-		console.log("Switching Theme!");
-		if (e.target.checked) {
-			document.documentElement.setAttribute("data-theme", "dark");
-			localStorage.setItem("theme", "dark"); //add this
-		} else {
-			document.documentElement.setAttribute("data-theme", "light");
-			localStorage.setItem("theme", "light"); //add this
-		}
+		const theme = e.target.checked ? "dark" : "light";
+		document.documentElement.setAttribute("data-theme", theme);
+		localStorage.setItem("theme", theme);
 	}
 
-	toggleSwitch.addEventListener("change", switchTheme, false);
+	// Add event listener to toggle switch
+	toggleSwitch.addEventListener("change", switchTheme);
 
-	const currentTheme = localStorage.getItem("theme")
-		? localStorage.getItem("theme")
-		: null;
-
+	// Set initial theme based on localStorage
 	if (currentTheme) {
 		document.documentElement.setAttribute("data-theme", currentTheme);
-
-		if (currentTheme === "dark") {
-			toggleSwitch.checked = true;
-		}
+		toggleSwitch.checked = currentTheme === "dark";
 	}
 });
