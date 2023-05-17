206ca5137a978b44049d80d4f01e2541c1fec9cd
Jeffrey Luu <83382029+jdluu@users.noreply.github.com>
2023-05-16 17:31:16 -0700

    refactor: remove unnecessary code

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,23 +4,8 @@ const { nav } = site;
 import "./Navbar.css";
 
 const Navbar = () => {
-	const [color, setColor] = useState(false);
-
-	useEffect(() => {
-		window.addEventListener("scroll", () => handleScroll);
-		return () => window.removeEventListener("scroll", handleScroll);
-	}, []);
-
-	function handleScroll() {
-		if (window.scrollY > 50) {
-			setColor(true);
-		} else {
-			setColor(false);
-		}
-	}
-
 	return (
-		<header id="header" className={color ? "active" : ""}>
+		<header id="header">
 			<ul id="topNavList">
 				{nav.map((item, i) => (
 					<li key={i}>
