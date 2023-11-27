7261bd151e59da3e204fa4d3da322285eae4720f
jdluu <j2013agl@gmail.com>
2023-11-26 20:23:48 -0800

    refactor: remove qwik

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import qwikdev from "@qwikdev/astro";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), qwikdev()],
+	integrations: [react()],
 });
