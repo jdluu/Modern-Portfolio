ca5eccfd31e30ec77c1300c9aea95e1cecb50b0e
jdluu <j2013agl@gmail.com>
2024-03-12 16:24:46 -0700

    chore: update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,10 @@
 import { defineConfig } from "astro/config";
-import storyblok from "@storyblok/astro";
 import { loadEnv } from "vite";
 import tailwind from "@astrojs/tailwind";
 import basicSsl from "@vitejs/plugin-basic-ssl";
-const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 export default defineConfig({
-	integrations: [
-		storyblok({
-			accessToken: env.STORYBLOK_TOKEN,
-			components: {
-				blogPost: "storyblok/BlogPost",
-				card: "storyblok/ProjectCard",
-				grid: "storyblok/ProjectGrid",
-			},
-			apiOptions: {
-				region: "us",
-			},
-		}),
-		tailwind(),
-	],
+	integrations: [tailwind()],
 	i18n: {
 		defaultLocale: "en",
 		locales: ["en", "fr", "pt-br", "es"],
