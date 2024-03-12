f93c9675b75f141fc988553f8f61a5a4d8d0a17d
jdluu <j2013agl@gmail.com>
2024-03-11 20:41:22 -0700

    feat: add storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,26 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [
+		tailwind(),
+		storyblok({
+			accessToken: env.STORYBLOK_TOKEN,
+			components: {
+				blogPost: "storyblok/BlogPost",
+				card: "storyblok/ProjectCard",
+				grid: "storyblok/ProjectGrid",
+			},
+			apiOptions: {
+				region: "us",
+			},
+		}),
+	],
 	i18n: {
 		defaultLocale: "en",
 		locales: ["en", "fr", "pt-br", "es"],
