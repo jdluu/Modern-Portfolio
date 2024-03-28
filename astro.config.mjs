06f9c728c4933c88a524c4c61788681474b33331
jdluu <j2013agl@gmail.com>
2024-03-27 17:24:40 -0700

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from "astro/config";
 import { loadEnv } from "vite";
 import tailwind from "@astrojs/tailwind";
 import basicSsl from "@vitejs/plugin-basic-ssl";
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	site: "https://jluu.dev",
+	integrations: [tailwind(), sitemap()],
 	i18n: {
 		defaultLocale: "en",
 		locales: ["en", "fr", "pt-br", "es"],
