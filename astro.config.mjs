4de6c232b418333945504cf386aa7adc0601d3e2
jdluu <j2013agl@gmail.com>
2024-06-09 14:39:38 -0700

    update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import { loadEnv } from "vite";
 import tailwind from "@astrojs/tailwind";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
@@ -11,11 +10,5 @@ export default defineConfig({
 	i18n: {
 		defaultLocale: "en",
 		locales: ["en", "fr", "pt-br", "es"],
-	},
-	vite: {
-		plugins: [basicSsl()],
-		server: {
-			https: true,
-		},
 	},
 });
