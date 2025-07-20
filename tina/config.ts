37c7adde76dfbbe19d239105da77d1a2e152bc47
jdluu <j2013agl@gmail.com>
2025-07-19 22:27:48 -0700

    Add TinaCMS search configuration with indexer token

--- tina/config.ts
+++ tina/config.ts
@@ -25,6 +25,11 @@ export default defineConfig({
 			publicFolder: "public",
 		},
 	},
+	search: {
+		tina: {
+			indexerToken: process.env.TINA_SEARCH_TOKEN,
+		},
+	},
 	// See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
 	schema: {
 		collections: [
