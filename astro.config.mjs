009039e1ca42c8e2f14d8a43212d8d91b703e00e
jdluu <j2013agl@gmail.com>
2025-07-20 02:05:07 -0700

    chore: implement code changes to enhance functionality and improve performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "astro/config";
 import { loadEnv } from "vite";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://jluu.dev",
-	integrations: [tailwind(), sitemap()],
+	integrations: [tailwind(), sitemap(), react()],
 	i18n: {
 		defaultLocale: "en",
 		locales: ["en", "fr", "pt-br", "es"],
