602f88920153d4fe921141ae094baec088761ab1
jdluu <j2013agl@gmail.com>
2025-07-25 19:52:27 -0700

    remove unused import

--- src/components/ui/ThemeToggleButton.tsx
+++ src/components/ui/ThemeToggleButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { toggleTheme } from "../../lib/theme-toggle";
 
 const SunIcon = ({ className }: { className?: string }) => (
