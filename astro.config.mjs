46f8f77e24012af46d1818878f8cc6c4c8c7244a
jdluu <j2013agl@gmail.com>
2025-08-03 01:16:02 -0700

    remove tailwindcss as it was unused except for one file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from "astro/config";
-import { loadEnv } from "vite";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://jluu.dev",
-    integrations: [tailwind(), sitemap(), solidJs()],
+    integrations: [sitemap(), solidJs()],
     i18n: {
         defaultLocale: "en",
         locales: ["en", "fr", "pt-br", "es"],
