46a11566a20b0684650bc6eab10b713ef3f73c18
jdluu <j2013agl@gmail.com>
2025-08-18 21:05:55 -0700

    build(config): explicitly set base to '/

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
     site: "https://jluu.dev",
+    base: "/",
     integrations: [
         sitemap(),
         solidJs({ include: ['src/components/ui/**/*.tsx'] }),
