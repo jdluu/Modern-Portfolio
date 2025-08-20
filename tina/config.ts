9551e05a1f30527491cd158b68d339be7fa2da32
jdluu <j2013agl@gmail.com>
2025-08-19 21:41:11 -0700

    remove unneeded variable

--- tina/config.ts
+++ tina/config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from "tinacms";
 
-// Your hosting provider likely exposes this as an environment variable
-const branch =
-	process.env.NEXT_PUBLIC_TINA_BRANCH ||
-	process.env.GITHUB_BRANCH ||
-	process.env.VERCEL_GIT_COMMIT_REF ||
-	process.env.HEAD ||
-	"main";
-
 export default defineConfig({
-	branch,
+	branch: "main",
 
 	// Get this from tina.io
 	clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
